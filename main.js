e5e0421e34f6c8deade3ab89f5bb8961dcbab9eb
Ollie Gutierrez <ollie.f.gutierrez@gmail.com>
2023-08-22 12:05:23 +0100

    Cleaned up main by removing default cube

--- main.js
+++ main.js
@@ -7,26 +7,18 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-// scene.add( cube );
-
 const gridHelper = new THREE.GridHelper( 1000, 20 );
 scene.add( gridHelper );
 
 let player = new Player();
 
-player.camera.position.set(0,2,0);
+player.camera.position.set(0,35,0);
 
 
 function animate() {
 	requestAnimationFrame( animate );
 
 	player.update();
-
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
 
 	renderer.render( scene, player.camera );
 }
