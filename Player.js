88ac97a67a1bc3fd5e5b79f9effd1c7048cd17b0
Ollie Gutierrez <ollie.f.gutierrez@gmail.com>
2023-08-22 10:55:57 +0100

    Added basic movement and wishDir calculations

--- Player.js
+++ Player.js
@@ -2,14 +2,16 @@ import * as THREE from 'three';
 
 class Player {
 	constructor() {
-		document.body.addEventListener("click", async () => {
+		document.body.addEventListener('click', async () => {
 			document.body.requestPointerLock();
 		});
+		document.body.addEventListener('keydown', (event) => this.update_wishDir(event), false);
 		document.body.addEventListener('mousemove', (event) => this.update_rotation(event), false);
         this.euler = new THREE.Euler(0, 0, 0, 'YXZ');
 		this.sensitivity = 0.01;
         this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 		this.lookDir = new THREE.Vector3(0, 0, 0);
+		this.wishDir = new THREE.Vector3(0, 0, 0);
 	}
 
 	update_rotation(event) {
@@ -27,6 +29,31 @@ class Player {
 			this.lookDir = new THREE.Vector3(0, 0, -1);
 			this.lookDir.applyQuaternion(this.camera.quaternion);
 		}
+	}
+
+	update_wishDir(event) {
+		this.wishDir.set(0, 0, 0);
+
+		switch (event.keyCode) {
+			case 87:	// W key
+				this.wishDir.z -= 1;
+				break;
+			case 65:	// A key
+				this.wishDir.x -= 1;
+				break;
+			case 83:	// S key
+				this.wishDir.z += 1;
+				break;
+			case 68:	// D key
+				this.wishDir.x += 1;
+				break;
+		}
+
+		this.wishDir.applyQuaternion(this.camera.quaternion);
+
+		this.wishDir.y = 0;
+
+		this.camera.position.add(this.wishDir);
 	}
 }
 
