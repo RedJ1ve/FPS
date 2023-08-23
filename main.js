6d377b06cc658f27ba30f1e3068fbe0262fd7692
Ollie Gutierrez <ollie.f.gutierrez@gmail.com>
2023-08-23 21:12:42 +0100

    Trying to implement gjk, not working, jump fixed

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import * as THREE from 'three';
 import Player from './Player.js'
+import Collision from './Collision.js';
 
 const scene = new THREE.Scene();
+const collision = new Collision();
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -14,13 +16,44 @@ let player = new Player();
 
 player.camera.position.set(0,35,0);
 
+const geometry1 = new THREE.BoxGeometry( 20, 35, 20 );
+const material1 = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+const mesh1 = new THREE.Mesh( geometry1, material1 );
+scene.add(mesh1);
+
+const geometry2 = new THREE.BoxGeometry(40, 40, 40);
+const material2 = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
+const mesh2 = new THREE.Mesh( geometry2, material2 );
+mesh2.position.set(0, 20, 300);
+scene.add(mesh2);
+
+const positionAttribute = mesh1.geometry.getAttribute( 'position' );
+
+const vertex = new THREE.Vector3();
+
+for ( let vertexIndex = 0; vertexIndex < positionAttribute.count; vertexIndex ++ ) {
+
+	vertex.fromBufferAttribute( positionAttribute, vertexIndex );
+
+	console.log(vertex)
+
+}
 
 function animate() {
 	requestAnimationFrame( animate );
 
 	player.update();
-
+	mesh1.position.set(player.camera.position.x, player.camera.position.y - 40, player.camera.position.z)
+	// console.log(collision.GJK(mesh1, mesh2));
 	renderer.render( scene, player.camera );
+
+	/* for ( let vertexIndex = 0; vertexIndex < positionAttribute.count; vertexIndex ++ ) {
+
+		vertex.fromBufferAttribute( positionAttribute, vertexIndex );
+	
+		console.log(vertex)
+	
+	} */
 }
 
 animate();
